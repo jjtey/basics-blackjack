98ae2043e625bd1987060005f64d6c7c5ad18f6c
jjtey <junjietey@gmail.com>
2022-06-24 00:10:35 +0800

    First base version

--- script.js
+++ script.js
@@ -6,19 +6,51 @@ var computerCard2 = "";
 var main = function (input) {
   var cardDeck = makeDeck();
   var shuffledDeck = shufflecardDeck(cardDeck);
-  // var gameState = drawCards;
-  // //can consider an option to "cut" the deck (to replicate actual game, otherwise inconsequential)
-  // //if game state = xx, deal the cards -> which is each player/dealer draws 2 cards
-  // while ((gameState = drawCards)) {
-  //   playerCard1 = shuffledDeck.pop();
-  //   computerCard1 = shuffledDeck.pop();
-  //   playerCard2 = shuffledDeck.pop();
-  //   computerCard1 = shuffledDeck.pop();
-  //   gameState = checkForBlackjack;
-  // }
-  //do we want to simulate the way cards are dealt?
-  //show the 2 cards
+  var gameState = "drawCards";
+
+  //can consider an option to "cut" the deck (to replicate actual game, otherwise inconsequential)
+  //if game state = xx, deal the cards -> which is each player/dealer draws 2 cards
+  if ((gameState = "drawCards")) {
+    playerCard1 = shuffledDeck.pop();
+    computerCard1 = shuffledDeck.pop();
+    playerCard2 = shuffledDeck.pop();
+    computerCard2 = shuffledDeck.pop();
+    gameState = "checkForBlackjack";
+    showCards();
+  }
+  console.log(playerCard1.rank);
+  console.log(playerCard2.rank);
+  console.log(computerCard1.rank);
+  console.log(computerCard2.rank);
   //check for any Blackjacks (immediate win unless draw)
+  if ((gameState = "checkForBlackjack")) {
+    if (
+      playerCard1.rank + playerCard2.rank == 21 &&
+      computerCard1.rank + computerCard2.rank == 21
+    ) {
+      gameState = "playerDraws";
+      return (
+        playerAndDealerHands +
+        `Both you and the dealer got a blackjack! <br><br><b>It's a draw!</b>`
+      );
+    } else if (playerCard1.rank + playerCard2.rank == 21) {
+      gameState = "playerWins";
+      return (
+        playerAndDealerHands + `You got a blackjack!<br><br> <b>You win!</b>`
+      );
+    } else if (computerCard1.rank + computerCard2.rank == 21) {
+      gameState = "playerLoses";
+      return (
+        playerAndDealerHands +
+        `The dealer got a blackjack! <br><br> <b>You lose!</b>`
+      );
+    } else {
+      gameState = "hitOrStand";
+    }
+    if ((gameState = "hitOrStand")) {
+    }
+  }
+
   //if no Blackjacks, player is prompted to 'hit' or 'stand'
   //player can keep 'hitting' until ready or 'stand' or if hand exceeds 21, immediately show both player/dealer hands -> can only draw or lose
   //computer will automatically 'hit' or 'stand' -> logic could be computer only 'stands' when hands is 18 and above
@@ -30,25 +62,25 @@ function makeDeck() {
   var suits = ["diamonds", "clubs", "hearts", "spades"];
   for (suitsIndex = 0; suitsIndex < suits.length; suitsIndex += 1) {
     var currentSuit = suits[suitsIndex];
-    console.log(`current suit: ${currentSuit}`);
     //loop through each suit & within each suit, loop through each rank
     //for ranks == 1, 11, 12, 13 -> allocate different name from their rank
     for (var currentRank = 1; currentRank <= 13; currentRank += 1) {
       var currentName = currentRank;
-      console.log(`current rank: ${currentRank}`);
+      var currentValue = currentRank;
       if (currentRank == 1) {
         currentName = "ace";
+        currentValue = 11;
       } else if (currentRank == 11) {
         currentName = "jack";
-        currentRank = 10;
+        currentValue = 10;
       } else if (currentRank == 12) {
         currentName = "queen";
-        currentRank = 10;
+        currentValue = 10;
       } else if (currentRank == 13) {
         currentName = "king";
-        currentRank = 10;
+        currentValue = 10;
       }
-      var card = { rank: currentRank, suit: currentSuit, name: currentName };
+      var card = { rank: currentValue, suit: currentSuit, name: currentName };
       cardDeck.push(card);
     }
   }
@@ -70,4 +102,8 @@ function shufflecardDeck(cardDeck) {
 function random(size) {
   //randomly picks from remaining cards
   return Math.floor(Math.random() * size);
+}
+
+function showCards() {
+  return (playerAndDealerHands = `You drew ${playerCard1.rank} & ${playerCard2.rank}.<br>The dealer drew ${computerCard1.rank} & ${computerCard2.rank}.<br><br>`);
 }
